425795fe1c27bd1114b2f448105dcc1d2150d446
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-03-27 18:47:03 +0000

    Set background for material display

--- raphael/src/Components/materials-glossary/materials-glossary.jsx
+++ raphael/src/Components/materials-glossary/materials-glossary.jsx
@@ -3,16 +3,20 @@ import styles from './materials-glossary.module.css'; //Import CSS styling
 // Import components
 import SearchBar from '../search-bar/search-bar';
 import DropDown from '../drop-down/drop-down';
+import MaterialsDisplay from '../materials-display/materials-display';
 
 export default function MaterialsComposition(){
     return(
         <div className={styles.materialsCompositioncontainer}>
             <h1>Materials Glossary🧵</h1>
-            <div className={styles.searchcontainer}>
-                {/* Search bar */}
-                <SearchBar/>
-                <DropDown options={['A-Z', 'Z-A', 'Water Consumption', 'Carbon Footprint']} defaultOption={'Sort'}/>
-                <DropDown  options={['Man-made', 'Natural']} defaultOption={'Filter'}/>
+            <div className={styles.materialsContainer}>
+                <div className={styles.searchcontainer}>
+                    {/* Search bar */}
+                    <SearchBar/>
+                    <DropDown options={['A-Z', 'Z-A', 'Water Consumption', 'Carbon Footprint']} defaultOption={'Sort'}/>
+                    <DropDown  options={['Man-made', 'Natural']} defaultOption={'Filter'}/>
+                </div>
+                <MaterialsDisplay/>
             </div>
         </div>
     );
