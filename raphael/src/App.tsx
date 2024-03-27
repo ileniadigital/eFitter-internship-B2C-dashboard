cc89b7047eb6e0d18818d683995db5c184a1433a
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-03-27 16:57:46 +0000

    Included all brands in Brands Drop Down Menu from JSON file

--- raphael/src/App.tsx
+++ raphael/src/App.tsx
@@ -1,21 +1,34 @@
 import './Components/general.css';
 import './Components/reset.css';
-//Import components
 import Navigation from "./Components/navigation/navigation";
 import Welcome from "./Components/welcome/welcome";
 import DropDown from './Components/drop-down/drop-down';
 import Sizes from './Components/sizes/sizes';
 import StyleGuru from './Components/style-guru/style-guru';
 import MaterialsGlossary from './Components/materials-glossary/materials-glossary';
+import { useEffect, useState } from 'react';
+
+// Import data
+import materialsData from './data/brands.json';
 
 export default function App() {
+  // State to store the options
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    // Extract brand names from materialsData array
+    const brandNames = materialsData.map(item => item.name);
+    setOptions(brandNames);
+  }, []);
+
   return (
     <div className="App">
       {/* Render navigation bar */}
       <Navigation />
       {/* Render welcome message */}
       <Welcome />
-      <DropDown options={['Zara', 'H&M']} defaultOption={'Select a brand'} />
+      {/* Render DropDown with fetched options */}
+      <DropDown options={options} defaultOption={'Select a brand'} />
       {/* Render size cards for each category */}
       <Sizes />
       {/* Style Guru */}
---
+++ raphael/src/Components/search-bar/search-bar.tsx
@@ -0,0 +1,50 @@
+// import styles from './search-bar.module.css'; // Import CSS styling
+// import { useRouter } from "next/router"; // Import router from next.js
+// import { useState, ChangeEvent } from "react";
+
+// interface iDefault {
+//     defaultValue: string | null;
+// }
+
+// export const SearchBar = ({ defaultValue }: iDefault) => {
+//     const router = useRouter();
+//     const [inputValue, setValue] = useState(defaultValue)
+
+//     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//         const inputValue = event.target.value
+//         setValue(inputValue);
+//     }
+
+//     const handleSearch = () => {
+//         if (router) {
+//             if (inputValue) {
+//                 return router.push(`/search/${inputValue}`)
+//             } else {
+//                 router.push(`/`)
+//             }
+//         } else {
+//             console.error("Router is not available."); // Handle the case when router is not available
+//         }
+//     }
+
+//     const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+//         if (event.key === 'Enter') {
+//             handleSearch();
+//         }
+//     }
+
+//     return (
+//         <div className={styles.searchBarContainer}>
+//             <label htmlFor="inputId">searchIcon</label>
+//             <input
+//                 type="text"
+//                 id="inputID"
+//                 placeholder="Search for a material"
+//                 value={inputValue ?? ""}
+//                 onChange={handleChange}
+//                 onKeyDown={handleKeyPress}
+//                 className={styles.search} // Use styles from CSS module
+//             />
+//         </div>
+//     );
+// }
