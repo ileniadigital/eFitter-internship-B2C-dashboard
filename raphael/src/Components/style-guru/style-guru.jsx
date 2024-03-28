419ab9f66e38c9ed5f033fef23747bc4d0bb0e8c
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-03-28 13:40:56 +0000

    Finished newsletter embed

--- raphael/src/Components/style-guru/style-guru.jsx
+++ raphael/src/Components/style-guru/style-guru.jsx
@@ -1,9 +1,23 @@
 import styles from './style-guru.module.css'; //Import CSS styling
 import { MdArrowDropDownCircle } from "react-icons/md";
+import { useState } from 'react';
+
+//Import image
+import newsletter from './newsletter-image.png';
+
 //Style Guru Persona component
-export default function StyleGuru(){
+export default function StyleGuru() {
     let stylegurupersona = "Free Spirit";
-    return(
+    
+    // Use state to manage the newsletterOpen state
+    const [newsletterOpen, setNewsletterOpen] = useState(false);
+
+    // Toggle the state of newsletterOpen
+    function toggleNewsletter() {
+        setNewsletterOpen(!newsletterOpen);
+    }
+
+    return (
         <div className={styles.styleGurucontainer}>
             <div className={styles.personaContainer}>
                 <h3 className={styles.persona}>Style Guru Persona</h3>
@@ -11,15 +25,24 @@ export default function StyleGuru(){
             <div className={styles.arrowContainer}>
                 {/* Style Guru Persona */}
                 <h2>{stylegurupersona}</h2>
-                {/* Drop Down Menu */}
-                <button className={styles.styleGuruarrow}><MdArrowDropDownCircle size={30} /></button>
+                {
+                    newsletterOpen ? (
+                        <button className={`${styles.styleGuruarrow} ${styles.rotated}`} onClick={toggleNewsletter}>
+                            <MdArrowDropDownCircle size={30} />
+                        </button>
+                    ) : (
+                        <button className={styles.styleGuruarrow} onClick={toggleNewsletter}>
+                            <MdArrowDropDownCircle size={30} />
+                        </button>
+                    )
+                }
             </div>
             {/* Newsletter */}
-            <div className={styles.newsletter}>
-                <p>Newsletter goes here</p>
-            </div>
-            
+            {newsletterOpen && (
+                <div className={styles.newsletter}>
+                    <img src={newsletter.src} alt="placeholder" />
+                </div>
+            )}
         </div>
     );
-
 }
