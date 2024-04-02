1caa665d585380a0c727da3800100514ca3dc9f5
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-04-02 10:31:58 +0100

    Started integration with backend

--- raphael/src/Components/materials-display/materials-display.tsx
+++ raphael/src/Components/materials-display/materials-display.tsx
@@ -1,10 +1,36 @@
 import styles from './materials-display.module.css'; //Importing CSS file
+import { useEffect, useState } from 'react';
+
+// Import data
+import { fetchProductCompositions } from '../../data/api';
 
 //MaterialsDisplay component to display materials 
 export default function MaterialsDisplay() {
+    const [materials, setMaterials] = useState([]);
+
+    // Fetch retailers data from the API
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchProductCompositions();
+                setMaterials(data);
+            } catch (error) {
+            }
+        };
+        fetchData();
+    }, []);
+
+    const options = materials.map((material) => (material.name));
+    console.log(options);
+
     return (
         <div className={styles.materialsContainer}>
             <h1>Materials Display</h1>
+            <ul>
+                {options.map((option, index) => (
+                    <li key={index}>{option}</li>
+                ))}
+            </ul>
         </div>
     )
 }
